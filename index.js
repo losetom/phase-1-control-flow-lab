3e4ff90a5371b9d304fc0768bb4cc005d48ad965
Mike Loseto <losetom1998@gmail.com>
2022-07-16 12:00:39 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue) {
+    if (someValue <= 400) {
+      return 'This one is on me!';
+    }
+    else if (someValue >= 400 && someValue <= 2000) {
+      return 'That will be twenty bucks.';
+    }
+    else if (someValue === 2001) {
+      return 'I will gladly take your thirty bucks.';
+    }
+    else if (someValue === 2501) {
+      return 'No can do.';
+    }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+function ternaryCheckCity(cityName) { 
+  let results =  (cityName === 'NYC') ?  'Ok, sounds good.' : 'No go.'
+  return results
+
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default: 
+      return 'Bye.'   
+  }
 }
